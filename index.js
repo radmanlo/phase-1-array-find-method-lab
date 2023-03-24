0f3bebbe44628876ffa4c76bb477794c80caaf70
radmanlo <radman.lotfiazar@gmail.com>
2023-03-24 13:04:44 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+
+function superbowlWin(records){
+    for (const record of records){
+        if (record.result === "W")
+            return record.year
+    }
+}
+
+//console.log(superbowlWin(record))
